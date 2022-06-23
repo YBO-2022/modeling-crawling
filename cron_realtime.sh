4951b9dd29b284eb8518e5db9fe2ea0727cb5a6b
hyunkyungju <wngusrudrid@naver.com>
2022-06-23 21:22:48 +0900

    chore: 크론 sh 파일 크론 폴더 내로 위치 변경

--- cron_realtime.sh
+++
@@ -1,9 +0,0 @@
-cron_path=~/ybo_cron
-crawling_realtime_path=${cron_path}/crawling-realtime
-
-echo "Start Realtime Crawling"
-
-python3 ${crawling_realtime_path}/game.py
-python3 ${crawling_realtime_path}/ranking.py
-
-echo "Finish Realtime Crawling"
