aad1b32dbb04b09fc858af006f440b2eb455a799
hyunkyungju <wngusrudrid@naver.com>
2022-06-23 21:21:10 +0900

    feat: 크론으로 수행할 쉘 스크립트 작성

---
+++ cron/cron_realtime.sh
@@ -0,0 +1,9 @@
+crawling_realtime_path=${CODE_PATH}/crawling-realtime
+
+echo "Start Realtime Crawling"
+
+python3 ${crawling_realtime_path}/game.py
+python3 ${crawling_realtime_path}/ranking.py
+
+echo "Finish Realtime Crawling"
+
