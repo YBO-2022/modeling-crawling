093b98b5235afb83e00eee0aead6cccb4d4f6558
hyunkyungju <wngusrudrid@naver.com>
2022-06-24 15:25:57 +0900

    feat: csv파일 정보를 rdb에 저장

---
+++ csv-to-rdb/ops_predict_csv_to_rdb.py
@@ -0,0 +1,46 @@
+import time
+import pandas as pd
+import pymysql
+from sqlalchemy import create_engine
+import sqlalchemy
+import configparser
+from dotenv import load_dotenv
+import os
+
+# 행: 100,000, 열: 40, 파일 크기: 27.9MB
+df = pd.read_csv("./predict_ops.csv", encoding='utf-8', usecols=['name', 'team', 'OPS', 'prediction_OPS'])
+df.columns = ['name', 'team' , 'ops', 'predict_ops']
+df['ops_predict_id'] = df.index
+
+# params
+user = os.getenv('DB_USERNAME')
+password = os.getenv('DB_PASSWORD')
+host = os.getenv('DB_HOST')
+port = 3306
+database = "ybo_db"
+
+
+# DB 접속 엔진 객체 생성
+engine = create_engine(f'mysql+pymysql://{user}:{password}@{host}:{port}/{database}', encoding='utf-8')
+
+# DB 테이블 명
+table_name = "ops_predict"
+
+dtypesql = {'ops_predict_id': sqlalchemy.types.Integer, 
+            'name': sqlalchemy.types.VARCHAR(255), 
+            'team': sqlalchemy.types.VARCHAR(255), 
+            'ops': sqlalchemy.types.Float,
+            'predict_ops': sqlalchemy.types.Float
+}
+
+# DB에 DataFrame 적재
+df.to_sql(index = False,
+          name = table_name,
+          con = engine,
+          if_exists = 'replace',
+          method = 'multi', 
+          chunksize = 10000,
+          dtype=dtypesql)
+
+with engine.connect() as con:
+    con.execute('ALTER TABLE `ops_predict` ADD PRIMARY KEY (`ops_predict_id`);')
