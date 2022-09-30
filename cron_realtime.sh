f829d1d13d56b9f42254f22abcd2ce3aedfa8512
hyunkyungju <wngusrudrid@naver.com>
2022-09-30 13:05:36 +0000

    chore: 실시간 크론 sh 순서 변경

--- cron_realtime.sh
+++ cron_realtime.sh
@@ -1,12 +1,10 @@
+#!/bin/sh
+echo "Start Realtime Crawling"
 
 CODE_PATH=$(dirname $(realpath $0))
-
 crawling_realtime_path=${CODE_PATH}/crawling-realtime
-
-echo "Start Realtime Crawling"
 
 python3 ${crawling_realtime_path}/game.py
 python3 ${crawling_realtime_path}/ranking.py
 
 echo "Finish Realtime Crawling"
-
